1b25c3a8a131da57f04802d8444f868b92b75f80
mehmetCt <mehmet@cybertekschool.com>
2023-03-24 20:36:55 -0400

    LibraryApp --> Database Testing Project --> TEMPLATE --> Mar 25

---
+++ src/test/java/com/library/steps/Hooks.java
@@ -0,0 +1,41 @@
+package com.library.steps;
+
+import com.library.utility.ConfigurationReader;
+import com.library.utility.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        System.out.println("this is coming from BEFORE");
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
+
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        System.out.println("this is coming from AFTER");
+
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+
+    }
+
+
+
+
+}
