2d140cd4cb8b40875d2396914d847f59d1371585
kamaryaa <kamaryaa12@gmail.com>
2023-03-29 14:43:30 -0400

    US06KAH  branch created

--- src/test/java/com/library/steps/AddNewBookStepDefs_KAH.java
+++ src/test/java/com/library/steps/AddNewBookStepDefs_KAH.java
@@ -10,7 +10,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
-public class AddNewBookStepDefs extends BasePage {
+public class AddNewBookStepDefs_KAH extends BasePage {
     LoginPage loginPage = new LoginPage();
     BookPage bookPage = new BookPage();
     String expectedBookName;
