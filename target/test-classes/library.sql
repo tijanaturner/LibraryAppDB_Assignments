590d9b0a915937e84d55808c7a29e9d3c4a79548
Busra_Cydeo <juliebt2012@gmail.com>
2023-03-29 15:48:22 -0400

    US07 completed

---
+++ target/test-classes/library.sql
@@ -0,0 +1,12 @@
+-- US01
+select count(id) from users;
+-- 1855
+
+select count(distinct id) from users;
+
+select count(*) from book_borrow
+where is_returned = '0';
+
+select count(*) from book_borrow
+where is_returned = '0';
+
