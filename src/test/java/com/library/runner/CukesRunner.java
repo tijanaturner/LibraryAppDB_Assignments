2a59d2f440ff67c6b0e784b06cf3ebcdc2f2037e
abdughopur02 <abdughopur02@gmail.com>
2023-03-30 10:20:22 -0400

    update my cukeRunner

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = false,
-        tags = "@US03_AA"
+        tags = ""
 )
 public class CukesRunner {
 
